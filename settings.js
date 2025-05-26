b2408cb49c3d8563c4ad6bc7256d500135622deb
sheoranbhai <sheoranbhai91@gmail.com>
2025-05-26 06:31:01 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: '𝐁ɼokᴇ᪱ɴ💔᪳𝆺꯭𝅥 Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "𝐁ɼokᴇ᪱ɴ💔᪳𝆺꯭𝅥  Bot",
+  botOwner: '🫧꯭꯭𝐂꯭꯭ ̶𝗮꯭𝗻d𝘆🍬̶̶꯭𝆭𝆬𝆭𝆬_*', // Your name
+  ownerNumber: '6282297283253', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
